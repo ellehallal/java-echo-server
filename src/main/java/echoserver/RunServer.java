e63f5fc9bcb259d283f8db82f46a8af7605388b2
Elle Hallal <daniellehallal@gmail.com>
2019-06-20 09:17:43 +0100

    Display: create class. ServerSocketWrapper initialises with display

--- src/main/java/echoserver/RunServer.java
+++ src/main/java/echoserver/RunServer.java
@@ -2,7 +2,8 @@ package echoserver;
 
 public class RunServer {
     public static void main(String[] args) {
-        var socket = new ServerSocketWrapper(5000);
+        var display = new Display();
+        var socket = new ServerSocketWrapper(8080, display);
         new EchoServer(socket).start();
     }
 }
